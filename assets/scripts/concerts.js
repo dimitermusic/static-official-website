2da55ec0a78f1e0b760dae010247164747ca0c5b
dimitermusic <dimiteryyordanov@gmail.com>
2022-02-21 15:17:52 -0800

    updated shows section with custom api js and css

---
+++ assets/scripts/concerts.js
@@ -0,0 +1,49 @@
+console.log('concerts.js linked');
+const API = 'https://dimiter-concert-api.herokuapp.com/api/concerts'
+
+fetch(API)
+    .then(res => res.json())
+    .then(data => {
+
+        console.log(data)
+
+        let tableBody = document.getElementById('concert-table');
+
+        if (data.length < 1) {
+            let comingSoon = document.createElement('p');
+            comingSoon.textContent = 'COMING SOON';
+            tableBody.appendChild(comingSoon);
+            tableBody.style.textAlign = 'center'
+        }
+
+        for (let i = 0; i < data.length; i++) {
+            let tableRow = document.createElement('div');
+            let date = document.createElement('p')
+            let venue = document.createElement('p')
+            let city = document.createElement('p')
+            let ticketBtn = document.createElement('a');
+
+            tableRow.classList.add('table-row')
+
+            ticketBtn.textContent = 'TICKETS';
+            ticketBtn.href = data[i].ticketLink;
+            date.textContent = data[i].date;
+            venue.textContent = data[i].venue;
+            city.textContent = data[i].city;
+
+
+            tableRow.appendChild(date);
+            tableRow.appendChild(venue);
+            tableRow.appendChild(city);
+            tableRow.appendChild(ticketBtn);
+            tableBody.appendChild(tableRow);
+
+
+        }
+
+    })
+    .catch(err => {
+        console.log('Error')
+        console.log(err)
+        return err
+    })
