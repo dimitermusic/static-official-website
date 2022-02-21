c22afe8757b2e36a0022b980db131cc923dc4098
dimitermusic <dimiteryyordanov@gmail.com>
2022-02-21 15:46:38 -0800

    completed custom concert widget

--- assets/scripts/concerts.js
+++ assets/scripts/concerts.js
@@ -17,16 +17,21 @@ fetch(API)
         }
 
         for (let i = 0; i < data.length; i++) {
-            let tableRow = document.createElement('div');
+            let tableRow = document.createElement('a');
             let date = document.createElement('p')
             let venue = document.createElement('p')
             let city = document.createElement('p')
             let ticketBtn = document.createElement('a');
 
             tableRow.classList.add('table-row')
+            ticketBtn.classList.add('btn')
+
+            tableRow.href = data[i].bandsInTownLink;
+            tableRow.target = '_blank'
 
             ticketBtn.textContent = 'TICKETS';
             ticketBtn.href = data[i].ticketLink;
+            ticketBtn.target = '_blank'
             date.textContent = data[i].date;
             venue.textContent = data[i].venue;
             city.textContent = data[i].city;
@@ -37,8 +42,6 @@ fetch(API)
             tableRow.appendChild(city);
             tableRow.appendChild(ticketBtn);
             tableBody.appendChild(tableRow);
-
-
         }
 
     })
