17dc9294c486e1707a279f3b2037e30875a4d92e
dimitermusic <dimiteryyordanov@gmail.com>
2022-02-23 16:15:54 -0800

    dynamic rendering of copyright year

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,56 +1,74 @@
-const API = 'https://dimiter-concert-api.herokuapp.com/api/concerts';
+console.log('hello world');
 
-fetch(API)
-    .then(res => res.json())
-    .then(data => {
+function concerts() {
 
-        let tableBody = document.getElementById('concert-table');
+    const API = 'https://dimiter-concert-api.herokuapp.com/api/concerts';
 
-        if (data.length < 1) {
+    fetch(API)
+        .then(res => res.json())
+        .then(data => {
 
-            let comingSoon = document.createElement('p');
-            comingSoon.textContent = 'COMING SOON!';
-            tableBody.appendChild(comingSoon);
-            comingSoon.style.textAlign = 'center'
+            let tableBody = document.getElementById('concert-table');
 
-        }
+            if (data.length < 1) {
 
-        for (let i = 0; i < data.length; i++) {
-
-            let tableRow = document.createElement('a');
-            let date = document.createElement('p')
-            let venue = document.createElement('p')
-            let city = document.createElement('p')
-            let ticketBtn = document.createElement('a');
-            let today = new Date().valueOf();
-            let currentConcert = new Date(data[i].date).valueOf();
-
-            if (currentConcert >= today) {
-
-                tableRow.classList.add('table-row')
-                ticketBtn.classList.add('btn')
-
-                tableRow.href = data[i].bandsInTownLink;
-                tableRow.target = '_blank'
-                ticketBtn.textContent = 'TICKETS';
-                ticketBtn.href = data[i].ticketLink;
-                ticketBtn.target = '_blank'
-                date.textContent = data[i].date;
-                venue.textContent = data[i].venue;
-                city.textContent = data[i].city;
-
-                tableRow.appendChild(date);
-                tableRow.appendChild(venue);
-                tableRow.appendChild(city);
-                tableRow.appendChild(ticketBtn);
-                tableBody.appendChild(tableRow);
+                let comingSoon = document.createElement('p');
+                comingSoon.textContent = 'COMING SOON!';
+                tableBody.appendChild(comingSoon);
+                comingSoon.style.textAlign = 'center'
 
             }
 
-        }
+            for (let i = 0; i < data.length; i++) {
 
-    })
-    .catch(err => {
-        console.log(err)
-        return err
-    })
+                let tableRow = document.createElement('a');
+                let date = document.createElement('p')
+                let venue = document.createElement('p')
+                let city = document.createElement('p')
+                let ticketBtn = document.createElement('a');
+                let today = new Date().valueOf();
+                let currentConcert = new Date(data[i].date).valueOf();
+
+                if (currentConcert >= today) {
+
+                    tableRow.classList.add('table-row')
+                    ticketBtn.classList.add('btn')
+
+                    tableRow.href = data[i].bandsInTownLink;
+                    tableRow.target = '_blank'
+                    ticketBtn.textContent = 'TICKETS';
+                    ticketBtn.href = data[i].ticketLink;
+                    ticketBtn.target = '_blank'
+                    date.textContent = data[i].date;
+                    venue.textContent = data[i].venue;
+                    city.textContent = data[i].city;
+
+                    tableRow.appendChild(date);
+                    tableRow.appendChild(venue);
+                    tableRow.appendChild(city);
+                    tableRow.appendChild(ticketBtn);
+                    tableBody.appendChild(tableRow);
+
+                }
+
+            }
+
+        })
+        .catch(err => {
+            console.log(err)
+            return err
+        })
+
+}
+
+function copyright() {
+
+    let copyrightText = document.getElementById('copyright-text')
+    let thisYear = new Date().toDateString().slice(11);
+
+    copyrightText.textContent = `© ${thisYear} Dimiter Yordanov. All Rights Reserved.`;
+
+}
+
+concerts();
+copyright();
