de44613d1578db62595612ac78af1b27bcfa6e65
dimitermusic <dimiteryyordanov@gmail.com>
2022-02-25 11:16:35 -0800

    update api link

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ console.log('hello world');
 // Fetch from custom concerts API and print to page
 const concerts = () => {
 
-    const API = 'https://dimiter-concert-api.wl.r.appspot.com/api/concerts';
+    const API = 'https://concert-api-5i4uylvzdq-wl.a.run.app/api/concerts';
 
     fetch(API)
         .then(res => res.json())
